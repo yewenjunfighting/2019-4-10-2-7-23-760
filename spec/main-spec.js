648afdd48a5a0d2777567d7dae4aa24c6e61948e
wenjun ye <1728914873@qq.com>
2019-05-21 20:45:56 +0800

    improve test cases

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -7,10 +7,28 @@ describe('taxi fee', function () {
         let expected = 7;
         expect(summary).toEqual(expected);
     });
+    it('within 2km and parking' , function() {
+        let inputs = {distance:1,parkTime:6};
+        let summary = main(inputs);
+        let expected = 8;
+        expect(summary).toEqual(expected);
+    });
+    it('within 2km and no parking', function() {
+        let inputs = {distance:2,parkTime:0};
+        let summary = main(inputs);
+        let expected = 6;
+        expect(summary).toEqual(expected)
+    })
     it('between 2km and 8km and parking' , function() {
         let inputs = {distance:7,parkTime:10};
         let summary = main(inputs);
         let expected = 13;
+        expect(summary).toEqual(expected);
+    });
+    it('between 2km and 8km and no parking' , function() {
+        let inputs = {distance:7,parkTime:0};
+        let summary = main(inputs);
+        let expected = 10;
         expect(summary).toEqual(expected);
     });
     it('more than 8km and parking' , function() {
